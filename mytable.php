9bb5e9416ae3e4a592a80bd50784da48d3207d08
aarongowins <jagowins@gmail.com>
2016-07-17 22:19:17 -0400

    ok

--- mytable.php
+++ mytable.php
@@ -12,13 +12,16 @@ if ($conn->connect_error) {
 } 
 
 // sql to create table
-$sql = "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP
-)";
+$sql = "CREATE TABLE `mytable` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1";
+
+
+
+
 
 if ($conn->query($sql) === TRUE) {
     echo "Table MyGuests created successfully";
